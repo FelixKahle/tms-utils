6ae193d2c0fbf13b2386e55453607515e23e4538
Felix Kahle <felix.kahle21@gmail.com>
2025-07-16 02:46:13 +0200

    Refactor XmlDeserializer: streamline initialization and improve buffer handling

--- crates/xmlprs/src/de.rs
+++ crates/xmlprs/src/de.rs
@@ -1,153 +1,148 @@
 // Copyright 2025 Felix Kahle. All rights reserved.
 
-use crate::opt::XmlDeserializerOptions;
-use quick_xml::events::Event;
+use quick_xml::events::{BytesStart, Event};
 use quick_xml::Reader;
 
-/// A deserializer for XML data using the `quick_xml` library.
-///
-/// This deserializer reads XML events from a byte slice and provides methods
-/// to read the next event and check the buffer capacity.
-///
-/// # Type Parameters
-///
-/// * `'a`: The lifetime of the input data, which is a byte slice containing XML data.
-///
-/// # Example
-///
-/// ```rust
-/// use xmlprs::de::XmlDeserializer;
-///
-/// let xml = br#"<Book><Title>Rust</Title></Book>"#;
-/// let deserializer = XmlDeserializer::new(xml);
-/// ```
 pub struct XmlDeserializer<'a> {
     reader: Reader<&'a [u8]>,
     buf: Vec<u8>,
 }
 
 impl<'a> XmlDeserializer<'a> {
-    /// The initial buffer size for the deserializer.
-    const INITIAL_BUF_SIZE: usize = 1024;
-
-    /// Creates a new `XmlDeserializer` from a byte slice containing XML data.
-    ///
-    /// # Examples
-    ///
-    /// ```rust
-    /// use xmlprs::de::XmlDeserializer;
-    ///
-    /// let xml = br#"<Book><Title>Rust</Title></Book>"#;
-    /// let mut deserializer = XmlDeserializer::new(xml);
-    /// ```
-    pub fn new(input: &'a [u8]) -> Self {
-        Self {
-            reader: Reader::from_reader(input),
-            buf: Vec::with_capacity(Self::INITIAL_BUF_SIZE),
-        }
-    }
-
-    /// Creates a new `XmlDeserializer` with a custom initial buffer capacity.
+    /// Creates a new `XmlDeserializer` from the given XML byte slice.
     ///
     /// # Arguments
     ///
-    /// * `input`: A byte slice containing the XML data to deserialize.
-    /// * `initial_buf_size`: The initial size of the buffer used for reading XML events.
+    /// * `xml`: A byte slice containing the XML data to be deserialized.
     ///
     /// # Returns
     ///
-    /// An instance of `XmlDeserializer` configured with the specified initial buffer size.
+    /// An instance of `XmlDeserializer` that can be used to read XML events.
     ///
     /// # Example
     ///
     /// ```rust
     /// use xmlprs::de::XmlDeserializer;
     ///
-    /// let xml = br#"<Book><Title>Rust</Title></Book>"#;
-    /// let mut deserializer = XmlDeserializer::with_initial_buffer_capacity(xml, 2048);
-    /// assert!(deserializer.next_event().is_ok());
+    /// let xml_data = br#"<root><element>Value</element></root>"#;
+    /// let deserializer = XmlDeserializer::new(xml_data);
     /// ```
-    pub fn with_initial_buffer_capacity(input: &'a [u8], initial_buf_size: usize) -> Self {
-        Self {
-            reader: Reader::from_reader(input),
-            buf: Vec::with_capacity(initial_buf_size),
+    pub fn new(xml: &'a [u8]) -> Self {
+        XmlDeserializer {
+            reader: Reader::from_reader(xml),
+            buf: Vec::new(),
         }
     }
 
-    /// Returns the current capacity of the internal buffer used for reading XML events.
+    /// Creates a new `XmlDeserializer` with a specified buffer capacity.
+    ///
+    /// # Arguments
+    ///
+    /// * `xml`: A byte slice containing the XML data to be deserialized.
+    /// * `capacity`: The initial capacity of the buffer used for reading XML events.
     ///
     /// # Returns
     ///
-    /// The capacity of the internal buffer in bytes.
+    /// An instance of `XmlDeserializer` with a buffer that can hold `capacity` bytes.
+    pub fn with_buffer_capacity(xml: &'a [u8], capacity: usize) -> Self {
+        XmlDeserializer {
+            reader: Reader::from_reader(xml),
+            buf: Vec::with_capacity(capacity),
+        }
+    }
+
+    /// Returns the current buffer capacity of the deserializer.
     ///
-    /// # Example
+    /// # Returns
     ///
-    /// ```rust
-    /// use xmlprs::de::XmlDeserializer;
-    ///
-    /// let xml = br#"<Book><Title>Rust</Title></Book>"#;
-    /// let deserializer = XmlDeserializer::new(xml);
-    /// assert_eq!(deserializer.buffer_capacity(), 1024);
-    /// ```
+    /// The current capacity of the internal buffer used for reading XML events.
     pub fn buffer_capacity(&self) -> usize {
         self.buf.capacity()
     }
 
-    /// Reads the next XML event from the input.
+    /// Reads the next XML event from the deserializer.
     ///
     /// # Returns
     ///
-    /// An `Option<Event>` where `Some(Event)` is the next XML event,
-    /// or `None` if the end of the XML input has been reached.
-    ///
-    /// # Example
-    ///
-    /// ```rust
-    /// use xmlprs::de::XmlDeserializer;
-    ///
-    /// let xml = br#"<Book><Title>Rust</Title></Book>"#;
-    /// let mut deser = XmlDeserializer::new(xml);
-    /// assert!(deser.next_event().is_ok());
-    /// ```
-    pub fn next_event(&mut self) -> quick_xml::Result<Option<Event>> {
+    /// A `quick_xml::Result<Event>` containing the next XML event or an error if one occurs.
+    pub fn next_event(&mut self) -> quick_xml::Result<Event> {
         self.buf.clear();
-        match self.reader.read_event_into(&mut self.buf)? {
-            Event::Eof => Ok(None),
-            evt => Ok(Some(evt)),
-        }
+        self.reader.read_event_into(&mut self.buf)
     }
 }
 
-/// Trait for types that can be deserialized from XML.
-///
-/// The `FromXml` trait provides a method to deserialize an instance
-/// of a type from XML data using a custom deserializer.
-///
-/// # Type Parameters
-///
-/// * `'de`: The lifetime of the deserializer.
-///
-/// # Example
-pub trait FromXml<'de>: Sized {
-    /// The error type returned by the deserializer.
-    ///
-    /// This is the type of error that can occur during deserialization.
+pub trait FromXml
+where
+    Self: Sized,
+{
     type Error;
 
-    /// Deserializes an instance of the type from XML data.
-    ///
-    /// # Arguments
-    ///
-    /// * `deserializer`: A mutable reference to an `XmlDeserializer` instance.
-    /// * `options`: An instance of `XmlDeserializerOptions` that
-    /// configures how XML elements and attributes are matched.
-    ///
-    /// # Returns
-    ///
-    /// A `Result` containing the deserialized instance of the type,
-    /// or an error if deserialization fails.
-    fn from_xml(
-        deserializer: &mut XmlDeserializer<'de>,
-        options: XmlDeserializerOptions,
-    ) -> Result<Self, Self::Error>;
+    fn from_xml<'a>(
+        opening_element: BytesStart<'a>,
+        deserializer: &mut XmlDeserializer<'a>,
+    ) -> quick_xml::Result<Self>;
+}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    struct Book {
+        title: String,
+        id: String,
+    }
+
+    impl Book {
+        fn new(title: String, id: String) -> Self {
+            Book { title, id }
+        }
+    }
+
+    impl FromXml for Book {
+        type Error = quick_xml::Error;
+
+        fn from_xml<'a>(
+            opening_element: BytesStart<'a>,
+            deserializer: &mut XmlDeserializer<'a>,
+        ) -> quick_xml::Result<Self> {
+            let mut title = String::new();
+            let mut id = String::new();
+
+            for attr in opening_element.attributes() {
+                let attr = attr;
+                match attr {
+                    Ok(a) if a.key.as_ref() == b"id" => {
+                        id = String::from_utf8(a.value.into_owned()).unwrap_or_default();
+                    }
+                    _ => continue,
+                }
+            }
+
+            loop {
+                match deserializer.next_event()? {
+                    Event::Text(e) => title = e.decode()?.to_string(),
+                    Event::End(e) => {
+                        return Ok(Book::new(title.clone(), id.clone()));
+                    }
+                    Event::Eof => panic!("Unexpected end of file while parsing XML"),
+                    _ => continue,
+                }
+            }
+        }
+    }
+
+    #[test]
+    fn test_from_xml_book() {
+        let xml_data = br#"<book id="12345">The Rust Programming Language</book>"#;
+        let mut deserializer = XmlDeserializer::new(xml_data);
+        let opening_element = match deserializer.next_event().unwrap() {
+            Event::Start(e) => e.into_owned(),
+            _ => panic!("Expected a Start event"),
+        };
+
+        let book = Book::from_xml(opening_element, &mut deserializer).unwrap();
+
+        assert_eq!(book.title, "The Rust Programming Language");
+        assert_eq!(book.id, "12345");
+    }
 }
--- crates/xmlprs/src/lib.rs
+++ crates/xmlprs/src/lib.rs
@@ -1,4 +1,3 @@
 // Copyright 2025 Felix Kahle. All rights reserved.
 
 pub mod de;
-pub mod opt;
